9fc1c8fdffa39116af92c92e21db87c226f2e99f
JuliaGrisales <jgrisales@gmail.com>
2020-10-09 17:44:10 +0200

    Editing a post

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Editar Artículo</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <form 
+                      action="{{ route('posts.update', $post) }}" 
+                      method="post" 
+                      enctype="multipart/form-data"
+                    >
+                      <div class="form-group">
+                        <label for="title">Título *</label>
+                        <input type="text" name="title" class="form-control" required value="{{ old('title', $post->title) }}">
+                      </div>
+                      <div class="form-group">
+                        <label for="file">Imagen</label>
+                        <input type="file" name="file">
+                      </div>
+                      <div class="form-group">
+                        <label for="body">Contenido *</label>
+                        <textarea name="body" rows="6" class="form-control" required>{{ old('boby', $post->body) }}</textarea>
+                      </div>
+                      <div class="form-group">
+                        <label for="iframe">Contenido embedido</label>
+                        <textarea name="iframe" rows="6" class="form-control" required>{{ old('iframe', $post->iframe) }}</textarea>
+                      </div>
+                      <div class="form-group">
+                        @csrf
+                        @method('PUT')
+                        <input type="submit" value="Actualizar" class="btn btn-sm btn-primary">
+                      </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
